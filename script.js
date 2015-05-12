3d4dede4a184c127f79eda3ebfee72bf0a4c6b31
lexmasta <rachelmetheny@Rachel-Methenys-MacBook-Pro.local>
2015-05-12 12:30:45 -0400

    some in class changes

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var main = function() {
   $('.nav li').click(function() {
     var category = $(this).attr('class');
-
-    $('.nav li').removeClass('active');
+// var category is the class of whatever we are clicking
+    $('.nav li').removeClass('active'); 
     $(this).addClass('active');
-
-    
-    
-    
+// basically making the nav bars pink when clicked and removing the pink from ones not clicked
+	$('.thumbnail').removeClass('selected');
+    if (category === 'nav-consumer') {
+    											// conditionals that add pink border to thumbnails that are clicked, and remove from other classes
+    	$('.consumer').addClass('selected');
+    }else if (category === 'nav-mobile') {
+    	
+    	$('.mobile').addClass('selected');
+    }else if (category === 'nav-enterprise') {
+    	
+    	$('.enterprise').addClass('selected');
+    }else if (category === 'nav-commerce') {
+    	
+    	$('.commerce').addClass('selected');
+    }		
   });
 };
  
 $(document).ready(main);
+// storing all behavior in function called main
+// important that it runs after page loaded 
